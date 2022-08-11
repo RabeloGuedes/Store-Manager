ed42719b827beb9f30372dbf562755f51c631be6
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-08-11 19:54:45 -0300

    feat: Importa as rotas a serem utilizadas pelo express.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const app = require('./app');
 require('dotenv').config();
+const productsRoutes = require('./routes/products');
+const productsControllers = require('./controllers/products');
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
 });
+
+app.use('/products', productsRoutes);
+
+app.get('/', productsControllers.getAllProducts);
