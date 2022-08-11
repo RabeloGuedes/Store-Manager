ff4623061b3d8ea3833955a2661f6143f73db653
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-08-11 19:51:45 -0300

    feat: Cria arquivo que realiza para requisições de todos os produtos ou um produto único pelo id.

---
+++ controllers/products.js
@@ -0,0 +1,18 @@
+const productsServices = require('../services/products');
+
+const getAllProducts = async (_req, res) => {
+  const allProducts = await productsServices.getAllProducts();
+  res.status(200).json(allProducts);
+};
+
+const getProductById = async (req, res) => {
+  const { id } = req.params;
+  const { response, code: { code } } = await productsServices.getProductById(id);
+  // const product = await productsServices.getProductById(id);
+  res.status(code).json(response);
+};
+
+module.exports = {
+  getAllProducts,
+  getProductById,
+};
