8f574625f568cec9111bb0bc95a8381aa3f7fb0f
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-08-12 20:59:16 -0300

    feat: Adiciona função que registra no banco de dados na tabela sales e sales_products uma nova compra.

---
+++ models/sales.js
@@ -0,0 +1,23 @@
+const connection = require('./connection');
+
+const createSale = async (body) => {
+  const date = new Date();
+  const currentDate = `${date.getFullYear()}-0${date.getMonth()}-0${date.getDay()}`;
+  const [newSale] = await connection.execute('INSERT INTO sales (date) VALUES (?)', [currentDate]);
+  body.forEach(async ({ productId, quantity }) => {
+    await connection.execute(`INSERT INTO sales_products
+    (product_id, sale_id, quantity)
+    VALUES (?, ?, ?)`, [productId, newSale.insertId, quantity]);
+  });
+  return newSale.insertId;
+};
+
+const getAllSales = async () => {
+  const [allSales] = await connection.execute('SELECT * FROM sales_products');
+  return allSales;
+};
+
+module.exports = {
+  createSale,
+  getAllSales,
+};
